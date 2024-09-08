75aa2680edac14e12e226de3487e829f6e0a8d57
bugcrusher07 <tarun.develops@gmail.com>
2024-09-08 13:16:34 +0530

    idk what i am even doing

---
+++ bruh.py
@@ -0,0 +1,3 @@
+a = [1,2,3]
+for i in range(0,4):
+  print(a[i])
--- tictactoe.py
+++ tictactoe.py
@@ -21,23 +21,36 @@ def player_turn (list):
   else:
     return o
 
-def move(player,list):
+def move(player,list,winner):
   input_line = int(input("enter the line\n"))
   element_number = int(input("enter the element number\n"))
   list[input_line][element_number]= player
-  who_won(input_line,element_number,list)
+  winner = who_won(input_line,element_number,list)
 
 def who_won(row,column,list):
   player = list[row][column]
   winner =  player
-  if row and column != 1:
-    if row or column == 1:
-      for index,element in list:
-        if index != row:
-          continue
-        for subElement in element:
-          if subElement != player:
-            winner = None
+  row_winner = player
+  column_winner = player
+  # if row and column != 1:
+  #   if row or column == 1:
+  #     for index,element in list:
+  #       if index != row:
+  #         continue
+  #       for subElement in element:
+  #         if subElement != player:
+  #           winner = None
+  if row and column !=1:
+    if row or column ==1:
+      for i in range(0,3):
+        if list[i][column] != player:
+          row_winner = None
+        elif list[row][i]!= player:
+          column_winner = None
+  if row_winner and column_winner == None:
+    winner = None
+  return winner
+
 
 
 
@@ -46,11 +59,8 @@ def main():
     winner = None
     turn = player_turn(modified_board_state)
     while turn != None:
-      move(turn,modified_board_state)
-      if counter <= 4:
-         counter +=1
-      else:
-         winner = who_won(modified_board_state)
+      move(turn,modified_board_state,winner)
+
 
       print(*modified_board_state,sep="\n")
       if winner != None:
